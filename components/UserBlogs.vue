5e5225e7c0eb2a1380be812cddc7532d3fd6c3ff
shrinathprabhu <shrinathprabhu.123@gmail.com>
2024-02-12 12:06:22 +0530

    Use inline flex to center blogs

--- components/UserBlogs.vue
+++ components/UserBlogs.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="flex-grow flex justify-center items-center">
     <div class="text-center">
-      <div class="p-4 rounded-full border-2 border-solid border-black">
+      <div
+        class="inline-flex p-4 rounded-full border-2 border-solid border-black"
+      >
         <img src="~/assets/images/icons/book-open.svg" class="w-20 h-20" />
       </div>
       <h2 class="mt-2 font-semibold">No blogs posted</h2>
